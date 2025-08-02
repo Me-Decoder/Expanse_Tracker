9d730ad78672509d0804771bbc62ae2032417499
sujalkatariya <sujalkatariya43359@gmail.com>
2025-08-02 12:22:25 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.google.gms.google.services)
+
+}
+
+android {
+    namespace = "com.example.expansetracker"
+    compileSdk = 35
+
+    buildFeatures{
+        viewBinding = true
+    }
+
+    defaultConfig {
+        applicationId = "com.example.expansetracker"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation (libs.material)   // latest stable version
+
+    // Firebase Firestore
+    implementation ("com.google.firebase:firebase-firestore-ktx:25.1.4")
+
+// Firebase Auth (if you want login)
+    implementation ("com.google.firebase:firebase-auth-ktx:23.2.1")
+
+// Material Design
+    implementation (libs.material.v1110)
+
+// RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.4.0")
+
+// Glide (for profile images later, optional)
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+    kapt ("com.github.bumptech.glide:compiler:4.16.0")
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
